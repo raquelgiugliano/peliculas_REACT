fd3d08034df1e452c18c15816d0048bc439c8a9f
raquelgiugliano <raquel.giugliano@gmail.com>
2023-06-05 17:19:07 +0200

    Peliculas

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { MyRoutes } from "./routers/routes";
+
+function App() {
+  return (
+    <header>
+      <h1 className="title">Películas🎬</h1>
+      <MyRoutes />
+    </header>
+  );
+}
+
+export default App;
---
+++ src/components/ContextMovieCard.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { get } from "../data/httpClient";
+import {MovieCard} from "./MovieCard"
+import "./ContextMovieCard.css";
+
+export function ContextMovieCard() {
+  const [movies, SetMovies] = useState([]);
+  useEffect(() => {
+    get("/discover/movie").then((data) => {
+      SetMovies(data.results);
+      console.log(data);
+    });
+  }, []);
+
+  return (<ul className="container">
+    {movies.map((movie)=>(
+        <MovieCard key={movie.id} movie={movie}/>
+    ))}  </ul>)
+}
